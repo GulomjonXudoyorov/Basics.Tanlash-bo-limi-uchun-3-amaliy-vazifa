98335645e6b359cf8d1493bc7f50b56f81be70d0
GulomjonXudoyorov <alisherov1ch0774@gmail.com>
2024-12-23 18:10:44 +0500

    Solved

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Gradusni kiriting: ");
+double degree = Convert.ToDouble(Console.ReadLine());
+double formulaFahrenheit = (degree * 9/5) + 32;
+double formulaKelvin =  degree + 273.15;
+Console.WriteLine($"Selsiyus: {degree}, Fahrenheit: {formulaFahrenheit}, Kelvin : {formulaKelvin}");
